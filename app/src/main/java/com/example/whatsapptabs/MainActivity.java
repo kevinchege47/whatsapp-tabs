098b8d989ec65bc2bc69304e5dc4c758e0d2052f
kevinchege47 <chegekevin001@gmail.com>
2023-02-01 13:02:44 +0300

    completed

---
+++ app/src/main/java/com/example/whatsapptabs/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.whatsapptabs;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tabLayout = findViewById(R.id.tab_layout);
+        viewPager = findViewById(R.id.viewpager);
+        tabLayout.setupWithViewPager(viewPager);
+        VPAdapter vpAdapter = new VPAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        vpAdapter.addFragment(new fragment1(),"CHATS");
+        vpAdapter.addFragment(new fragment_2(),"STATUS");
+        vpAdapter.addFragment(new fragment_3(),"CALLS");
+        viewPager.setAdapter(vpAdapter);
+    }
+}
